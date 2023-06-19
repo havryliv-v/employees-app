c9ce6faaecbb21202d393ac41c8ceed949a728c3
Vitalii <havryliv.v@icloud.com>
2023-06-19 17:58:28 +0300

    Functions delete and add empl was added

--- src/components/empl-list/empl-list.js
+++ src/components/empl-list/empl-list.js
@@ -1,11 +1,11 @@
 import EmployeesListItem from "../empl-list-item/empl-list-item";
 import './empl-list.css';
 
-const EmployeesList = ({ data }) => {
-
+const EmployeesList = ({ data, onDelete }) => {
    const elements = data.map(item => {
+      const { id, ...itemProps } = item
       return (
-         <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} />
+         <EmployeesListItem key={id}{...itemProps} onDelete={() => { onDelete(id) }} />
       )
    })
 
